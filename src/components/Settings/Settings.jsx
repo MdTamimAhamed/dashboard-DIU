c05e1690046a4af6bc111d54af766b568fe5e9af
MdTamimAhamed <tamimssgts999@gmail.com>
2023-01-03 16:05:36 +0600

    test

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div className="ml-8 text-gray w-full">
+    <div className="ml-8 text-gray ">
         <h1 className=" bg-white w-1/2 h-[300px] p-5">Settings</h1>
     </div>
   )
--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Support = () => {
   return (
-    <div className="ml-8 text-gray w-full">
+    <div className="ml-8 text-gray ">
         <h1 className=" bg-white w-1/2 h-[300px] p-5">Support</h1>
     </div>
   )
