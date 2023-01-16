5e925928ed2c622ef83f94382deb1fe05b3af80a
Nabil <nabilaltamash@gmail.com>
2023-01-16 17:14:20 +0600

    Authentication works now, just login

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../context/Store';
 
 const Dashboard = () => {
-  return (
-    <div className="ml-8 text-gray ">
-        <h1 className=" bg-white w-1/2 h-[300px] p-5">This is Dashboard</h1>
-    </div>
-  )
-}
+  const { token, role, retrieveDataFromLocalStorage } = useContext(AuthContext);
 
-export default Dashboard
+  useEffect(() => {
+    retrieveDataFromLocalStorage();
+  }, []);
+
+  return (
+    <div className='ml-8 text-gray '>
+      <h1 className=' bg-white w-1/2 h-[300px] p-5'>This is Dashboard</h1>
+      <pre>{JSON.stringify({ token, role }, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/context/AuthReducer.js
+++ src/context/AuthReducer.js
@@ -1,14 +1,31 @@
-import {SET_PROFILE, SET_TOKEN, SET_ROLE} from "./Types";
-const AuthReducer = (state,action)=>{
-    switch(action.type){
-        case `${SET_PROFILE}`:
-            return;
-        case `${SET_TOKEN}`:
-            return;
-        case `${SET_ROLE}`:
-            return;
-        default:
-            return;
-    }
+import { SET_PROFILE, SET_TOKEN, SET_ROLE, SET_ERROR, LOGIN } from './Types';
+
+function AuthReducer(state, { type, payload }) {
+  switch (type) {
+    case SET_PROFILE:
+      return { ...state, profile: payload };
+    case SET_TOKEN:
+      return { ...state, token: payload.token };
+    case SET_ROLE:
+      return { ...state, role: payload.role };
+    case LOGIN:
+      return {
+        ...state,
+        token: payload.token,
+        role: payload.role,
+        authenticated: true
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: {
+          msg: payload?.msg || '',
+          err: payload?.err || {}
+        }
+      };
+    default:
+      return { ...state };
+  }
 }
+
 export default AuthReducer;
